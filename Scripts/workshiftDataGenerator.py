1df5064e8d6ada2f1ca82f500943ed36119a1dfb
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-05-13 00:15:05 +0300

    A first version describing the data insertion phase was added.

--- Scripts/workshiftDataGenerator.py
+++ Scripts/workshiftDataGenerator.py
@@ -11,7 +11,10 @@ for i in range(200):
     end_time = start_time + datetime.timedelta(hours=8)
 
     # Generate SQL insert statement
-    sql_insert = f"INSERT INTO WorkShift (start_time, end_time) VALUES (TO_TIMESTAMP('{start_time.strftime('%Y-%m-%d %H:%M:%S')}', 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP('{end_time.strftime('%Y-%m-%d %H:%M:%S')}', 'YYYY-MM-DD HH24:MI:SS'));"
+    sql_insert = f"INSERT INTO WorkShift (start_time, end_time)\
+    VALUES (TO_TIMESTAMP('{start_time.strftime('%Y-%m-%d %H:%M:%S')}',\
+        'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP('{end_time.strftime('%Y-%m-%d %H:%M:%S')}',\
+            'YYYY-MM-DD HH24:MI:SS'));"
 
     # Append SQL insert statement to list
     sql_inserts.append(sql_insert)
@@ -23,3 +26,5 @@ with open ('insertWorkShiftInfo.sql', "w") as file:
     # Print SQL insert statements
     for sql in sql_inserts:
         file.write(sql + '\n')
+
+
