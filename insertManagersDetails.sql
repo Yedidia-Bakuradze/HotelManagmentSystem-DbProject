40fc12b01f8f2ac5a1e9a71a83dfcf568f167a40
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-05-12 12:39:08 +0300

    Script selecting the managers from the Employee stack, so the script inserting the managers into Manager entity were added,.

---
+++ insertManagersDetails.sql
@@ -0,0 +1,8 @@
+insert into Manager (id,departId) values (613140988,151);
+insert into Manager (id,departId) values (464124768,152);
+insert into Manager (id,departId) values (741957854,153);
+insert into Manager (id,departId) values (218627085,154);
+insert into Manager (id,departId) values (876744169,155);
+insert into Manager (id,departId) values (190055515,156);
+insert into Manager (id,departId) values (782940781,151);
+insert into Manager (id,departId) values (800735183,153);
