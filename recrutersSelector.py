e9a762d6e88b289843f4502ff89e6415ea5e6312
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-05-12 12:44:14 +0300

    Script selecting the recruters (Human resources) from the Employee stack, so the script inserting the recruters into Recruter entity were added.

--- recrutersSelector.py
+++ recrutersSelector.py
@@ -1,13 +1,13 @@
 # Open the file containing the SQL queries
 with open('insertEmpDetails.sql', 'r') as infile:
     # Open the new file where the filtered queries will be written
-    with open('insertManagersDetails.sql', 'w') as outfile:
+    with open('insertRecrutersDetails.sql', 'w') as outfile:
         # Read each line (SQL query) in the input file
         for line in infile:
             # Check if the line contains 'Management'
-            if 'Management' in line:
+            if 'Human Resources' in line:
                 # Extract the id value
                 id_value = line.split('values')[1].split(',')[0].strip("'")
                 print(id_value)
                 # Write a new SQL query into the output file
-                outfile.write(f"insert into Manager (id, departId) values ({id_value});\n")
+                outfile.write(f"insert into Recruter (id) values ('{id_value}');\n")
