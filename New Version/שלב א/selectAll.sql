cd40f8f029e39aa951eeef230d4fe73f4f7464ff
Yedidia Bakuradze <yedidiadev@gmail.com>
2024-05-24 16:56:50 +0300

    Export file been fully implemented and now works properly.

---
+++ New Version/שלב א/selectAll.sql
@@ -0,0 +1,26 @@
+select *
+from Shift;
+
+select *
+from Department;
+
+select *
+from Position;
+
+select *
+from Employee;
+
+select *
+from Manager;
+
+select *
+from Training;
+
+select *
+from LeaveRequest;
+
+select *
+from EmployeeShift;
+
+select *
+from EmployeeTraining;
